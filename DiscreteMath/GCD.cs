b60b95ed28f91e3ea00cfe95f2c323197ea1a794
muffinman007 <cs70falls218@hotmail.com>
2015-05-29 16:59:44 -0700

    Add new project DiscreteMath for math library

---
+++ DiscreteMath/GCD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DiscreteMath{
+
+	/// <summary>
+	/// Greatest Common Divisor
+	/// </summary>
+	public class GCD{
+	}
+}
--- LibraryMaker/Program.cs
+++ LibraryMaker/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// playground to test libraries
+
 namespace LibraryMaker {
 	class Program {
 		static void Main(string[] args) {
